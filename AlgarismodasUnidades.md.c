419cc03e4cfe6369af1beab266b3c0fea03eb924
Leda Trindade <141334677+ledatrindade@users.noreply.github.com>
2023-09-28 12:45:45 -0300

    Update AlgarismodasUnidades.md.c
    Função resto '%'

--- AlgarismodasUnidades.md.c
+++ AlgarismodasUnidades.md.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
     int numero;
-    scanf("%d", &numero);
+        scanf("%d", &numero);
     numero=numero%10;
-    printf("Algarismo das unidades: %d", numero);
+        printf("Algarismo das unidades: %d", numero);
     return 0;
 }
 
