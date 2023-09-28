1a98f62bbd45edc8d9aac9eba89f499306f1b00f
Leda Trindade <141334677+ledatrindade@users.noreply.github.com>
2023-09-28 09:44:23 -0300

    Create AlgarismodasUnidades.md.c
    Questão 1

---
+++ AlgarismodasUnidades.md.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int numero;
+    scanf("%d", &numero);
+    numero=numero%10;
+    printf("Algarismo das unidades: %d", numero);
+    return 0;
+}
+
+RESULTADO:
+Entrada(s) para teste: 3872 	
+Saída do programa: 2	
