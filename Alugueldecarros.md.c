d4a1f2db6084cfc97f85bbb12be5901c8ffacfc9
Leda Trindade <141334677+ledatrindade@users.noreply.github.com>
2023-09-28 09:54:40 -0300

    Create Alugueldecarros.md.c
    Entrada(s) para teste: 7 e 174	
    Saída do seu programa: Valor a pagar pelo aluguel: R$ 190.566000

---
+++ Alugueldecarros.md.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    double x; double y; double valordias; double valorkm; double g; double h; double z;
+    scanf("%lf", &x);
+    scanf("%lf", &y);
+    valordias=30*x;
+    valorkm=0.01*y;
+    g=valordias+valorkm;
+    h=g*0.1;
+    z=g-h;
+    printf("Valor a pagar pelo aluguel: R$ %lf", z);
+return 0;
+}
