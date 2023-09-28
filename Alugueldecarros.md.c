9f884782ea598692602ee36b1c37d886bc32a801
Leda Trindade <141334677+ledatrindade@users.noreply.github.com>
2023-09-28 12:42:31 -0300

    Update Alugueldecarros.md.c
    Nesse código foi trabalhado a manipulação e métodos de com usar a variável tipo DOUBLE.

--- Alugueldecarros.md.c
+++ Alugueldecarros.md.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+
 int main(){
     double x; double y; double valordias; double valorkm; double g; double h; double z;
-    scanf("%lf", &x);
-    scanf("%lf", &y);
+        scanf("%lf", &x);
+        scanf("%lf", &y);
     valordias=30*x;
     valorkm=0.01*y;
     g=valordias+valorkm;
     h=g*0.1;
     z=g-h;
-    printf("Valor a pagar pelo aluguel: R$ %lf", z);
+        printf("Valor a pagar pelo aluguel: R$ %lf", z);
 return 0;
 }
